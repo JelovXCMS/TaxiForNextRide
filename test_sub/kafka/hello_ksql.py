87d415b01688a39e12722606fdbb7e289259f324
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2020-02-05 22:01:23 +0000

    ksql test

--- test_sub/kafka/hello_ksql.py
+++ test_sub/kafka/hello_ksql.py
@@ -2,5 +2,5 @@ import logging
 from ksql import KSQLAPI
 
 logging.basicConfig(level=logging.DEBUG)
-client = KSQLAPI('ec2-52-41-32-196.us-west-2.compute.amazonaws.com:8088')
+client = KSQLAPI('http://ec2-52-41-32-196.us-west-2.compute.amazonaws.com:8088')
 
